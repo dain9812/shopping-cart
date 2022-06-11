26764f57c2509e65eb7a47c314c6becb1a20397b
hamdain <dain9812@naver.com>
2022-06-11 17:51:49 +0900

    feat: 기본 components 셋팅

---
+++ src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.nav`
+  position: fixed;
+  left: 0;
+  right: 0;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Logo = styled.h1`
+  font-size: 30px;
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  font-size: 35px;
+  cursor: pointer;
+`;
+
+const Nav = () => {
+  return (
+    <Container>
+      <Inner>
+        <Logo>Shop</Logo>
+        <Button>🛒</Button>
+      </Inner>
+    </Container>
+  );
+};
+
+export default Nav;
