edb27a7b7991e066d3c628b6df5d473ac265fd53
hamdain <dain9812@naver.com>
2022-06-14 05:03:14 +0900

    feat: 총 결재금액 리듀서 생성

--- src/components/Cart/CartList.jsx
+++ src/components/Cart/CartList.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { calculateTotalPrice } from "../../store/cartSlice";
 import CartListItem from "./CartListItem";
 
 const Cart = styled.div`
@@ -20,19 +21,32 @@ const Cart = styled.div`
   }
 `;
 
-const CloseBtn = styled.button`
+const Button = styled.button`
   background: none;
   border: none;
+  cursor: pointer;
+`;
+
+const CloseButton = styled(Button)`
   position: fixed;
   top: 20px;
   right: -100%;
   font-size: 28px;
   color: #777;
-  cursor: pointer;
   transition: right 0.5s;
   &.open {
     right: 20px;
   }
+`;
+
+const SubmitButton = styled(Button)`
+  width: 100%;
+  padding: 10px 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  margin-top: 20px;
+  background: #3953ff;
+  color: #fff;
 `;
 
 const Inner = styled.div`
@@ -47,8 +61,28 @@ const ListUl = styled.ul`
   padding: 30px 0;
 `;
 
+const Result = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+  border-top: 8px solid #eee;
+`;
+
+const TotalPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Num = styled.span`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
 const CartList = ({ open, setOpen }) => {
-  const list = useSelector((state) => state.cart.list);
+  const dispatch = useDispatch();
+  const { list, totalPrice } = useSelector((state) => state.cart);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -56,18 +90,34 @@ const CartList = ({ open, setOpen }) => {
     console.log(list);
   }, [list]);
 
+  useEffect(() => {
+    dispatch(calculateTotalPrice(items));
+  }, [dispatch, items]);
+
   return (
     <Cart className={open ? "open" : ""}>
-      <CloseBtn className={open ? "open" : ""} onClick={() => setOpen(false)}>
+      <CloseButton
+        className={open ? "open" : ""}
+        onClick={() => setOpen(false)}
+      >
         ✕
-      </CloseBtn>
+      </CloseButton>
       <Inner>
         <Title>Cart</Title>
         <ListUl>
-          {items.map((item) => (
-            <CartListItem item={item} key={item.id} />
-          ))}
+          {items.length > 0
+            ? items.map((item) => <CartListItem item={item} key={item.id} />)
+            : "카트에 담긴 상품이 없습니다"}
         </ListUl>
+        <Result>
+          <TotalPrice>
+            <span>결제금액</span>
+            <span>
+              <Num>{totalPrice}</Num> 원
+            </span>
+          </TotalPrice>
+          <SubmitButton>결제하기</SubmitButton>
+        </Result>
       </Inner>
     </Cart>
   );
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   list: [],
+  totalPrice: 0,
 };
 
 export const cartSlice = createSlice({
@@ -22,9 +23,33 @@ export const cartSlice = createSlice({
         state.list.push({ ...action.payload, count: 1 });
       }
     },
+    removeItem: (state, action) => {
+      const existItemIndex = state.list.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      state.list[existItemIndex] = {
+        ...state.list[existItemIndex],
+        count: state.list[existItemIndex].count - 1,
+      };
+    },
+    clearItem: (state, action) => {
+      state.list = [
+        ...state.list.filter((item) => item.id !== action.payload.id),
+      ];
+    },
+    calculateTotalPrice: (state) => {
+      const initialPrice = 0;
+      const price = state.list.reduce(
+        (prev, cur) => prev + cur.price * cur.count,
+        initialPrice
+      );
+      state.totalPrice = price;
+    },
   },
 });
 
-export const { addItem } = cartSlice.actions;
+export const { addItem, removeItem, clearItem, calculateTotalPrice } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
